9958157cc222f63a308cfdd84b89eb0a7da999fc
Lars554 <larsdewinter5@gmail.com>
2026-01-20 11:27:42 +0100

    yyy

---
+++ testdingmicrochip/testdingmicrochip/main.c
@@ -0,0 +1,18 @@
+/*
+ * testdingmicrochip.c
+ *
+ * Created: 20/01/2026 11:27:21
+ * Author : larsd
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
