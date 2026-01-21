cf2127b878034b77be20d327ba7aa93379aedc55
Lars554 <larsdewinter5@gmail.com>
2026-01-21 10:12:17 +0100

    tèèt

--- testdingmicrochip/testdingmicrochip/main.c
+++ testdingmicrochip/testdingmicrochip/main.c
@@ -10,12 +10,7 @@
 #include <util/delay.h>
 #include <util/twi.h>
 
-#define DC   DDD2
-#define RST  DDD3
-#define CS   DDD4
-#define OLED_WIDTH 128
-#define OLED_HEIGHT 64
-#define SPIF0 7 
+
 
 void init_gpio()
 {
@@ -49,15 +44,6 @@ void init_uart(void)
 	// TXEN0 = enable transmitter
 }
 
-// SPI init voor ATmega328PB
-void spi_init(void){
-    // PB0 = MOSI, PB1 = SCK, PB2 = SS als output
-    DDRB |= (1<<PB0)|(1<<PB1)|(1<<PB2);
-
-    // SPI master, fosc/16
-    SPCR0 = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
-}
-
 uint16_t ADC_read(uint8_t channel)
 {
     ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
@@ -80,89 +66,13 @@ void UART_send_number(uint16_t num) {
 	UART_send_char('\n');
 }
 
-// SPI write byte
-void spi_write(uint8_t data){
-    SPDR0 = data;
-    while(!(SPSR0 & (1<<SPIF0)));
-}
 
-// OLED control functions
-void oled_command(uint8_t cmd){
-    PORTD &= ~(1<<DC);  // DC=0 -> command
-    PORTD &= ~(1<<CS);  // CS low
-    spi_write(cmd);
-    PORTD |= (1<<CS);   // CS high
-}
-
-void oled_data(uint8_t data){
-    PORTD |= (1<<DC);   // DC=1 -> data
-    PORTD &= ~(1<<CS);  // CS low
-    spi_write(data);
-    PORTD |= (1<<CS);   // CS high
-}
-
-// Reset OLED
-void oled_reset(void){
-    PORTD &= ~(1<<RST);
-    _delay_ms(10);
-    PORTD |= (1<<RST);
-    _delay_ms(10);
-}
-
-// Basic SSD1306 init sequence
-void oled_init(void){
-    oled_reset();
-    _delay_ms(50);
-
-    oled_command(0xAE); // Display off
-    oled_command(0xD5); oled_command(0x80); // Set display clock
-    oled_command(0xA8); oled_command(0x3F); // Set multiplex
-    oled_command(0xD3); oled_command(0x00); // Set display offset
-    oled_command(0x40); // Start line
-    oled_command(0x8D); oled_command(0x14); // Charge pump
-    oled_command(0x20); oled_command(0x00); // Memory mode
-    oled_command(0xA1); // Segment remap
-    oled_command(0xC8); // COM scan dec
-    oled_command(0xDA); oled_command(0x12); // COM pins
-    oled_command(0x81); oled_command(0xCF); // Contrast
-    oled_command(0xD9); oled_command(0xF1); // Pre-charge
-    oled_command(0xDB); oled_command(0x40); // VCOM detect
-    oled_command(0xA4); // Resume display RAM
-    oled_command(0xA6); // Normal display
-    oled_command(0xAF); // Display on
-}
-
-// Clear OLED (writes 0 to all pixels)
-void oled_clear(void){
-    for(uint16_t i=0;i<OLED_WIDTH*OLED_HEIGHT/8;i++){
-        oled_data(0x00);
-    }
-}
-
-// Draw a simple character (8x8 font)
-void oled_draw_char(uint8_t c){
-    // Dummy 8x8 font: just a block for example
-    for(uint8_t i=0;i<8;i++){
-        oled_data(0xFF);
-    }
-}
-
-// Draw "Hello" on screen
-void oled_draw_hello(void){
-    const char* str = "HELLO";
-    while(*str){
-        oled_draw_char(*str++);
-    }
-}
 
 int main(void)
 {
 	init_gpio();
 	init_pwm();
 	ADC_init();
-	spi_init();
-    oled_init();
-    oled_clear();
     _delay_ms(100);
     /* Replace with your application code */
     while (1) 
